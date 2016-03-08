1bc75308c8c637dd67f6c43a41c8a519014c2487
Calvin Lang <langcalvin@gmail.com>
2016-03-08 15:09:08 -0600

    Making API call, but there are troubles with Google updating since 2013

--- skullbuilder_tutorial/baseball_map.groovy
+++ skullbuilder_tutorial/baseball_map.groovy
@@ -1,5 +1,6 @@
 // following second half of this tutorial https://www.youtube.com/watch?v=-mRlc_vjiqE
 
+// First we have to build a stadium class/instance
 class Stadium {
 	int id
 	String name
@@ -8,4 +9,23 @@ class Stadium {
 	double longitude
 
 	String toString() {"($name,$latitude,$longitude"}
+
+	def base = 'https://maps.googleapis.com/maps/api/geocode/json?'
+	def params = [sensor:false, oe:'utf8', output: 'json']
+	def fillInLatLong() {
+		def url = base + name.split(" ").join("+")
+		println url
+	}
 }
+
+def stadiums = []
+stadiums << new Stadium(name:'Angel Stadium',team:'ana')
+stadiums << new Stadium(name:'Chase Field',team:'ari')
+stadiums << new Stadium(name:'Turner Field',team:'atl')
+stadiums << new Stadium(name:'Oriole Field',team:'bal')
+stadiums << new Stadium(name:'Rogers Centre',team:'tor')
+
+
+// URL: https://maps.googleapis.com/maps/api/geocode/output?parameters
+
+stadiums.each { s -> s.fillInLatLong()}
