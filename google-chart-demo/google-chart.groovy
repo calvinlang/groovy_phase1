be2b4ee0dc8f078ed3950e8e33e30d9c01a9cb83
Calvin Lang <langcalvin@gmail.com>
2016-03-08 13:46:31 -0600

    Google Chart API works

--- google-chart-demo/google-chart.groovy
+++ google-chart-demo/google-chart.groovy
@@ -4,7 +4,7 @@ def base = 'http://chart.apis.google.com/chart?'
 // I'm confused on the syntax as this is reffered to as a "map"
 // I'm getting the impression that 
 def params = [cht: 'p3' , chs: '250x100',
-                chd: 't:60,40' , ch1: 'Hello World']
+                chd: 't:60,40' , ch1: 'Hello|World']
 
 // collect accepts a block/closure. Collect seems to me like map or .each
 // the $ sign is a shorthanded string interpolation "g-strings"               
@@ -18,15 +18,14 @@ url.toURL().text
 import javax.swing.ImageIcon
 import javax.swing.WindowConstants as WC // as WC acts as a callable shorthand
 import java.awt.BorderLayout as BL
-import groovy.swing.Swingbuilder
+import groovy.swing.SwingBuilder
 
 SwingBuilder.build() {
-    frame(title:'Hello, World!', visibl:true,
+    frame(title:'Hello, World!', visible:true,
         defaultCloseOperation:WC.EXIT_ON_CLOSE) {
         label(icon:new ImageIcon(new URL(url)),
             constraints:BL.CENTER)
         }.pack()
-}
 }
 
 
